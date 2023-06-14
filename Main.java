db6e4ed0054988badedc64ef3b76acda0ef8bcc1
Gudykson <127319413+Gudykson@users.noreply.github.com>
2023-06-14 13:15:12 +0000

    Dziala
    chyba

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
-class Main {
+import java.util.Scanner;
+
+public class Main {
+  
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("podaj lancuch znakow");
+    String s = scanner.nextLine();
+    
+
+    System.out.println("podaj znak do wymiany");
+    
+
+    String znak = scanner.nextLine();
+  
+    
+    System.out.println("podaj nowy znak");
+
+    
+    String znak2 = scanner.nextLine();
+
+    
+    String s2 = s.replace(znak, znak2);
+    
+        System.out.println("Twój nowy lancuch to: "+ s2);
+
   }
 }
